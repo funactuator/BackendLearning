1512522df0e0f7f14f1b46a5d95f3d567460e540
Kuldeep Pandey <kuldeep.pandey@spyne.ai>
2022-10-09 16:08:16 +0530

    initial commit

---
+++ majorTakeAways.md
@@ -0,0 +1,5 @@
+1. How to create a server
+2. How to make it online(listen request)
+3. How to send simple text as reponse
+4. How to send simple html tags as reponse
+5. How to send full html file as reponse
---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require("http");
+const fs = require("fs");
+
+//create a new server instance and attach a callback function to it which gets called whenever a request is coming to it
+const server = http.createServer((req, res) => {
+    // console.log(`------------------------request object----------------------------`)
+    // console.log(req);
+    // console.log(`------------------------response object----------------------------`)
+    // console.log(res);
+
+    // ----------Sending plain text------------------
+    // res.setHeader('content-type', 'text/plain');
+    // res.write('Hello World is ours to rule!');
+    // res.end();
+
+    // -------------Sending html tags--------------
+    // res.setHeader('content-type', 'text/html');
+    // res.write('<h1>Hello Welcome to world of backend <h1>');
+    // res.write('<h2>You are gonna love it <h2>');
+    // res.end();
+
+    // --------------Serving html files----------------
+    fs.readFile('./views/index.html', (err, fileData) => {
+        res.setHeader('content-type', 'text/html');
+        res.write(fileData);
+        res.end();
+    })
+})
+
+//above code has created server and made the reaction callback but it has not started yet. 
+
+//listen method takes port number, hostname(domain name), and a callback method which gets called when server is online
+server.listen(3000, 'localhost', () => {
+    console.log("server is up darling")
+})
+
